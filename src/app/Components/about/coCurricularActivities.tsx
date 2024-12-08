361321ddb1c18ecef7fef2afe317dd27082d7fe6
AjayThorat-20 <ajaythorat988@gmail.com>
2024-12-08 21:45:25 +0530

    14th commit

--- src/app/Components/about/coCurricularActivities.tsx
+++ src/app/Components/about/coCurricularActivities.tsx
@@ -1,3 +1,67 @@
+// "use client";
+
+// import React from "react";
+
+// const ActivityData = [
+//   {
+//     id: 1,
+//     Name: "Ignite and Concat",
+//     description: "A program to foster collaboration and innovation.",
+//   },
+//   {
+//     id: 2,
+//     Name: "ISR and DLLE (Social Activity)",
+//     description: "Community engagement activities promoting social responsibility.",
+//   },
+//   {
+//     id: 3,
+//     Name: "SkillFull Netizen",
+//     description: "A workshop on becoming skilled and responsible digital citizens.",
+//   },
+//   {
+//     id: 4,
+//     Name: "Initiated and implemented 16-day sprints during the internship",
+//     description: "Streamlined project timelines through efficient sprint planning.",
+//   },
+// ] as const;
+
+// const CoCurricularActivities: React.FC = () => {
+//   return (
+//     <div className="mt-10 h-full px-4 ">
+//       {/* Header */}
+//       <div className="text-left mb-8">
+//         <h1 className="text-[2rem] md:text-[2.5rem] font-bold">
+//           Co-Curricular Activities
+//         </h1>
+//       </div>
+
+//       {/* Activities Grid */}
+//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-4 ">
+//         {ActivityData.map((activity) => (
+//           <div
+//             key={activity.id}
+//             className=" bg-gray-100 rounded-xl shadow-lg overflow-hidden p-6 mx-auto w-full"
+//           >
+//             <h3 className="text-black font-semibold text-lg md:text-xl">
+//               {activity.Name}
+//             </h3>
+//             <p className="mt-2 text-sm md:text-base text-black">
+//               {activity.description}
+//             </p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CoCurricularActivities;
+
+
+
+
+
+
 "use client";
 
 import React from "react";
@@ -6,22 +70,22 @@ const ActivityData = [
   {
     id: 1,
     Name: "Ignite and Concat",
-    description: "A program to foster collaboration and innovation.",
+    description: "During my bachelor's studies at *Patkar Varde College, Goregaon West, Mumbai, I actively contributed to my college's event team, showcasing my organizational and leadership skills. I was a proud member of the **marketing team* for two prominent events: *Concat, an intra-college event organized by the Computer Science Department, and **Ignite*, an inter-college event collaboratively hosted by the Computer Science and Information Technology Departments. My primary role involved spearheading efforts to secure sponsorships, financial support, and partnerships for these events. By collaborating with businesses and organizations, I successfully brought in funds, promotional materials, and sponsorship deals that significantly elevated the events' scale and success. This experience honed my communication, teamwork, and planning skills, while also fostering a strong sense of responsibility.",
   },
   {
     id: 2,
     Name: "ISR and DLLE (Social Activity)",
-    description: "Community engagement activities promoting social responsibility.",
+    description: "During my academic journey, both at *Patkar Varde College, Goregaon West, Mumbai, during my bachelor's studies and at **Hiray College of Computer Applications* during my master's, I actively participated in various *social activities* through *ISR (Institutional Social Responsibility)* and *DLLE (Department of Lifelong Learning and Extension)* initiatives. These activities included *tree plantation drives, **beach cleaning campaigns*, and programs aimed at empowering senior citizens by teaching them essential computer skills. These experiences not only allowed me to contribute positively to society but also enhanced my sense of responsibility, teamwork, and commitment to making a meaningful impact.",
   },
   {
     id: 3,
     Name: "SkillFull Netizen",
-    description: "A workshop on becoming skilled and responsible digital citizens.",
+    description: "As part of my co-curricular activities at *Patkar Varde College, Goregaon West, Mumbai, I participated in the **SkillFull Netizen* program from November 18, 2019, to November 28, 2019. This 10-day program focused on helping senior citizens learn the basics of computer usage. I had the opportunity to assist *Ms. Mariam*, a 52-year-old participant, as she navigated through various digital tasks. I guided her in creating and sending emails, using Microsoft Word, Excel, and PowerPoint, exploring Google search, working with Paint, installing software, and understanding basic hardware components. This experience strengthened my communication and teaching skills while also allowing me to contribute to helping senior citizens become more comfortable with technology.",
   },
   {
     id: 4,
     Name: "Initiated and implemented 16-day sprints during the internship",
-    description: "Streamlined project timelines through efficient sprint planning.",
+    description: "During a 16-day sprint from September 2nd to September 17th at *ShypBUDDY India Pvt Ltd, I made significant contributions while gaining valuable experience with **Agile methodology* in a startup environment. Leading the team, I focused on achieving key objectives, including designing efficient and user-friendly *UI/UX*, integrating third-party logistics systems, and optimizing the overall system. I also performed logic-building tasks, created database schemas, and wrote backend logic to ensure seamless functionality. Additionally, I optimized CRUD operations to enhance database performance and reduce request loads. Throughout this sprint, I streamlined project timelines through efficient sprint planning, which enhanced team productivity and project execution. These contributions showcased my technical expertise, leadership abilities, and adaptability in delivering impactful solutions within a structured Agile framework.",
   },
 ] as const;
 
@@ -36,7 +100,7 @@ const CoCurricularActivities: React.FC = () => {
       </div>
 
       {/* Activities Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-4 ">
+      <div className="grid grid-cols-1 gap-8 ">
         {ActivityData.map((activity) => (
           <div
             key={activity.id}
@@ -45,7 +109,7 @@ const CoCurricularActivities: React.FC = () => {
             <h3 className="text-black font-semibold text-lg md:text-xl">
               {activity.Name}
             </h3>
-            <p className="mt-2 text-sm md:text-base text-black">
+            <p className="mt-2 text-sm md:text-base text-black justify-center text-justify indent-8">
               {activity.description}
             </p>
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,107 @@
+// "use client";
+// import { useEffect, useState } from "react";
+// import WelcomeToProject from "./Components/projects/welcomeToProject";
+// import AcademicProjects from "./Components/projects/academicProjects";
+// import Image from "next/image";
+// import Link from "next/link";
+
+// export default function Home() {
+//   const [showRest, setShowRest] = useState(false);
+//   const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+//   useEffect(() => {
+//     if (isImageLoaded) {
+//       setShowRest(true);  // Show the rest of the content after image loads
+//     }
+//   }, [isImageLoaded]);
+
+//   return (
+//     <div className="text-center justify-center text-dark w-full h-screen lg:px-32 overflow-y-auto px-4 overflow-x-hidden hide-scrollbar py-10 md:pb-0">
+//       <div className="animate-slide-up">
+//         <div className="w-full h-full text-center items-center justify-center space-y-6 pb-10">
+//           <div className="flex flex-col md:flex-row items-center md:justify-between w-full h-full space-y-8 md:space-y-0 md:px-0 px-0">
+//             <div className="relative w-full h-[100vh] md:w-1/2 md:h-[90vh] max-h-[85vh] ">
+//               {/* <Image
+//                 src="/Images/Profile/Ajay.png"
+//                 alt="Ajay Thorat - Professional Profile"
+//                 fill
+//                 priority
+//                 placeholder="blur"
+//                 blurDataURL="data:image/jpeg;base64,..."
+//                 className={`md:object-cover object-contain md:scale-120 brightness-70 transition-opacity duration-500 ${isImageLoaded ? 'opacity-100' : 'opacity-0'}`}
+//                 onLoad={() => setIsImageLoaded(true)}
+//                 sizes="(max-width: 768px)  100vw, 50vw"
+//               /> */}
+
+
+// <Image
+//   src="/Images/Profile/Ajay.png"
+//   alt="Ajay Thorat - Professional Profile"
+//   fill
+//   priority
+//   placeholder="blur"
+//   blurDataURL="data:image/jpeg;base64,..."
+//   className={`md:object-contain object-contain scale-100 border-gray-400 md:scale-120 brightness-70 transition-opacity duration-500 ${isImageLoaded ? 'opacity-100' : 'opacity-0'}`}
+//   onLoad={() => setIsImageLoaded(true)}
+//   sizes="(max-width: 768px) 100vw, 50vw"
+// />
+
+//             </div>
+
+//             {showRest && (
+//               <div className="w-full md:w-1/2 h-full text-center md:text-left space-y-6 animate-slide-up order-2 md:order-last px-4 md:px-0 pb-20">
+//                 <div className="space-y-6">
+//                   <h1 className="text-2xl md:text-[2.6rem] font-bold mb-4">
+//                     Aspiring to learn and apply.
+//                   </h1>
+//                   <p className="text-base md:text-[1.2rem] mb-6 text-justify indent-6">
+//                     A dedicated and self-motivated full-stack developer driven by a passion for solving real-world problems. Committed to contributing to national, organizational, and personal growth through innovative solutions, adaptability, and a relentless pursuit of excellence.
+//                   </p>
+//                 </div>
+//                 <div className="flex justify-center md:justify-start">
+//                   <Link
+//                     href="/Resume/Ajay_Thorat.pdf"
+//                     target="_blank"
+//                     className="inline-block bg-black text-white px-4 py-2.5 rounded-lg text-base md:text-lg hover:bg-white hover:text-black border-2 border-solid border-transparent hover:border-black"
+//                   >
+//                     Resume
+//                   </Link>
+//                 </div>
+//               </div>
+//             )}
+//           </div>
+//         </div>
+//       </div>
+
+
+
+
+
+
+
+
+
+
+
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 "use client";
 import { useEffect, useState } from "react";
 import WelcomeToProject from "./Components/projects/welcomeToProject";
@@ -20,38 +124,26 @@ export default function Home() {
       <div className="animate-slide-up">
         <div className="w-full h-full text-center items-center justify-center space-y-6 pb-10">
           <div className="flex flex-col md:flex-row items-center md:justify-between w-full h-full space-y-8 md:space-y-0 md:px-0 px-0">
-            <div className="relative w-full h-[100vh] md:w-1/2 md:h-[90vh] max-h-[85vh] ">
-              {/* <Image
-                src="/Images/Profile/Ajay.png"
-                alt="Ajay Thorat - Professional Profile"
-                fill
-                priority
-                placeholder="blur"
-                blurDataURL="data:image/jpeg;base64,..."
-                className={`md:object-cover object-contain md:scale-120 brightness-70 transition-opacity duration-500 ${isImageLoaded ? 'opacity-100' : 'opacity-0'}`}
-                onLoad={() => setIsImageLoaded(true)}
-                sizes="(max-width: 768px)  100vw, 50vw"
-              /> */}
+          <div className="relative w-full h-[40vh] md:w-1/2 md:h-[85vh] max-h-[90vh] ">
+  <Image
+    src="/Images/Profile/Ajay.png"
+    alt="Ajay Thorat - Professional Profile"
+    fill
+    priority
+    placeholder="blur"
+    blurDataURL="data:image/jpeg;base64,..."
+    className={`md:object-contain object-contain scale-100 border-gray-400 md:scale-120 brightness-70 transition-opacity duration-500 ${isImageLoaded ? 'opacity-100' : 'opacity-0'} 
+        md:rounded-none rounded-full shadow-xl w- md:shadow-none`} // rounded-full for mobile screens
+    onLoad={() => setIsImageLoaded(true)}
+    sizes="(max-width: 768px) 100vw, 50vw"
+  />
+</div>
 
-
-<Image
-  src="/Images/Profile/Ajay.png"
-  alt="Ajay Thorat - Professional Profile"
-  fill
-  priority
-  placeholder="blur"
-  blurDataURL="data:image/jpeg;base64,..."
-  className={`md:object-contain object-contain scale-100 border-gray-400 md:scale-120 brightness-70 transition-opacity duration-500 ${isImageLoaded ? 'opacity-100' : 'opacity-0'}`}
-  onLoad={() => setIsImageLoaded(true)}
-  sizes="(max-width: 768px) 100vw, 50vw"
-/>
-
-            </div>
 
             {showRest && (
-              <div className="w-full md:w-1/2 h-full text-center md:text-left space-y-6 animate-slide-up order-2 md:order-last px-4 md:px-0 pb-20">
-                <div className="space-y-6">
-                  <h1 className="text-2xl md:text-[2.6rem] font-bold mb-4">
+              <div className="w-full md:w-1/2 h-full text-center md:text-left space-y-6 animate-slide-up order-2 md:order-last px-4 md:px-0 pb-20 md:pb-0">
+                <div >
+                  <h1 className="text-2xl md:text-[2.6rem] font-bold mb-4 ">
                     Aspiring to learn and apply.
                   </h1>
                   <p className="text-base md:text-[1.2rem] mb-6 text-justify indent-6">
