144185ff536f0c374c77d4f1f1f9205eac597d3a
AjayBThorat-20 <ajaythorat988@gmail.com>
2025-02-18 11:26:09 +0530

    new commit after warpping home and about to wrapperLayout

--- src/Components/Layout/wrapperLayout.tsx
+++ src/Components/Layout/wrapperLayout.tsx
@@ -11,10 +11,13 @@ export default function WrapperLayout({ firstPosition, secondPosition, children 
     <div className="px-6 sm:px-10 lg:px-16">
       {/* Header */}
       <div className="py-8">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-center mb-8 sm:mb-12 text-gray-800 dark:text-gray-200">
-          <span>{firstPosition}</span>{" "}
-          <span className="text-blue-500 dark:text-blue-400">{secondPosition}</span>
-        </h1>
+
+        {firstPosition || secondPosition ? (
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-center mb-8 sm:mb-12 text-gray-800 dark:text-gray-200">
+            <span>{firstPosition}</span>{" "}
+            <span className="text-blue-500 dark:text-blue-400">{secondPosition}</span>
+          </h1>
+        ) : null}
 
         {/* Children */}
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,60 @@
+// "use client";
+
+// import { useSelector } from "react-redux";
+// import { RootState } from "@/store/store";
+// import { BasicInfo, OrbImage } from "@/Components/Home/page";
+
+// export default function Home() {
+//   const theme = useSelector((state: RootState) => state.theme.mode); // Access `mode` instead of `value`
+
+//   return (
+//     <div className="flex flex-col md:flex-row items-center md:justify-between w-full h-full space-y-8 md:space-y-0 ">
+      
+//       <div className="relative w-full h-[55vh] md:w-1/2 md:h-[85vh] max-h-[85vh]">
+//         <OrbImage
+//           theme={theme}
+//           src="/Images/Profile/Ajay.png"
+//           alt="Person wearing VR headset"
+//           className="brightness-100"
+          
+//         />
+//       </div>
+//       <BasicInfo theme={theme} />
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
 "use client";
 
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import { BasicInfo, OrbImage } from "@/Components/Home/page";
-
+import WrapperLayout from "@/Components/Layout/wrapperLayout";
 export default function Home() {
-  const theme = useSelector((state: RootState) => state.theme.mode); // Access `mode` instead of `value`
+  const theme = useSelector((state: RootState) => state.theme.mode);
 
   return (
-    <div className="flex flex-col md:flex-row items-center md:justify-between w-full h-full space-y-8 md:space-y-0 ">
-      
-      <div className="relative w-full h-[55vh] md:w-1/2 md:h-[85vh] max-h-[85vh]">
-        <OrbImage
-          theme={theme}
-          src="/Images/Profile/Ajay.png"
-          alt="Person wearing VR headset"
-          className="brightness-100"
-          
-        />
+    <WrapperLayout firstPosition="" secondPosition="">
+      <div className="flex flex-col md:flex-row items-center md:justify-between w-full h-full space-y-8 md:space-y-0">
+        <div className="relative w-full h-[55vh] md:w-1/2 md:h-[85vh] max-h-[85vh]">
+          <OrbImage
+            theme={theme}
+            src="/Images/Profile/Ajay.png"
+            alt="Person wearing VR headset"
+            className="brightness-100"
+          />
+        </div>
+        <BasicInfo theme={theme} />
       </div>
-      <BasicInfo theme={theme} />
-    </div>
+    </WrapperLayout>
   );
 }
